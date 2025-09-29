e9b0c558514f014b0e49bb8ce396ee268fec5a11
willalone <159647207+willalone@users.noreply.github.com>
2025-09-29 21:27:07 +0300

    added seo files

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/realty/',
+  base: './',
   optimizeDeps: {
     include: ['react', 'react-dom'],
     esbuildOptions: { target: 'es2020' },
